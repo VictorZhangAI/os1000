09e4beabf0cb7bb09e54989dfe606b8aeca6c564
VZstless <2319725508@qq.com>
2025-02-03 17:23:28 +0800

    added printf in the OS

--- common.c
+++ common.c
@@ -12,7 +12,57 @@ void printf(const char *fmt, ...)
 		if(*fmt == '%')
 		{
 			fmt++;
+			switch(*fmt)
+			{
+				case '\0':
+					putchar('%');
+					goto end;
+				case '%':
+					putchar('%');
+					goto end;
+				case 's':{
+					const char *s = va_arg(vargs, const char *);
+			       		while(*s)
+					{
+							putchar(*s);
+							s++;
+					}
+					break;		
+				}
+				case 'd':{
+					int value = va_arg(vargs, int);
+					if(value < 0){
+						putchar('-');
+						value = -value;
+					}
 
-		}	
+					int divisor = 1;
+				 	while(value / divisor > 9)
+						divisor *= 10;
+					 
+					while(divisor > 0)
+					{
+						putchar('0' + value / divisor);
+						value %= divisor;
+						divisor /= 10;
+					}
+					break;
+				}
+				case 'x':{
+					int value = va_arg(vargs, int);
+			       		for(int i = 7; i >= 0; i--)
+					{
+						int nibble = (value >> (i * 4)) & 0xF;
+						putchar("0123456789abcdef"[nibble]);
+					}
+				}
+
+			}
+		} else{
+			putchar(*fmt);
+		}
+		fmt++;	
 	}
+end:
+	va_end(vargs);
 }
--- kernel.c
+++ kernel.c
@@ -1,4 +1,5 @@
 #include "kernel.h"
+#include "common.h"
 
 typedef unsigned char uint8_t;
 typedef unsigned int uint32_t;
@@ -43,9 +44,8 @@ void kernel_main(void)
 {
 	memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
 
-	const char *s = "\n\nHello, world!\n";
-	for(int i = 0; s[i] != '\0'; i++)
-		putchar(s[i]);
+	printf("\n\nHello %s\n", "World!");
+	printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
 	for(;;)
 		__asm__ __volatile__("wfi");
 }
